f824dc07fafe333ad1ad903c95cef12d9501ebd9
Julia Veronica <julia.veronicagdu@gmail.com>
2020-04-08 21:34:15 +0700

    table error

--- script.js
+++ script.js
@@ -1,38 +1,64 @@
-let data =[];
+let data = [];
 axios.get('http://localhost:3000/contacts')
-.then((response)=>{
-    console.log(response)
-    const listContact = document.querySelector("#contacts>ol")
-    data = response.data;
+    .then((response) => {
+        console.log(response)
+        const listContact = document.getElementById("#contacts")
+        data = response.data;
 
-    response.data.forEach(item =>{
-        const {id, name, address, email, phone, company} = item;
-        const itemHTML = `
-        <li>
-        Id: ${id}
-        <br>
-        Name: ${name}
-        <br>
-        Address: ${address}
-        <br>
-        Email: ${email}
-        <br>
-        Phone: ${phone}
-        <br>
-        Company: ${company}
-        <br>
-        <button onclick ="ganti(${id})">Change</button>
-        <button onclick ="hapus(${id})">Delete</button>
-        </li>
+        response.data.forEach(item => {
+            const {
+                id,
+                name,
+                address,
+                email,
+                phone,
+                company
+            } = item;
+            //div ini untuk id card
+            const itemHTML = `
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                    <img class="circularsquare" src="./4.jpg" alt=""
+                        style="width: 100px; height: 100px;">
+                    <div class="ccenter">
+                    <li>
+                    Id: ${id}
+                    <br>
+                    Name: ${name}
+                    <br>
+                    Address: ${address}
+                    <br>
+                    Email: ${email}
+                    <br>
+                    Phone: ${phone}
+                    <br>
+                    Company: ${company}
+                    <br>
+                    <button onclick ="ganti(${id})">Change</button>
+                    <button onclick ="hapus(${id})">Delete</button>
+                    </li>
+                    </div>
+                </div>
+       </div>
+       </div>
+       <tr id="contacts">
+       <th>${id}</th>
+       <td>${name}</td>
+       <td>${address}</td>
+       <td>${email}</td>
+       <td>${phone}</td>
+       <td>${company}</td>
+     </tr>
         `;
-        listContact.innerHTML +=itemHTML;
+            listContact.innerHTML += itemHTML;
+        })
     })
-})
-.catch((pesanError)=>{
-    console.log(pesanError);
-});
+    .catch((pesanError) => {
+        console.log(pesanError);
+    });
 
-document.getElementById('simpanContact').addEventListener('submit', function(event){
+document.getElementById('simpanContact').addEventListener('submit', function (event) {
     // event.preventDefault();
 
     const name = document.getElementById('name').value;
@@ -42,34 +68,34 @@ document.getElementById('simpanContact').addEventListener('submit', function(eve
     const company = document.getElementById('company').value;
     //untuk post
     const data = {
-        name : name,
-        address : address,
-        email : email, 
-        phone : phone,
-        company : company
+        name: name,
+        address: address,
+        email: email,
+        phone: phone,
+        company: company
     }
-    axios.post('http://localhost:3000/contacts',data).then( res => {
+    axios.post('http://localhost:3000/contacts', data).then(res => {
         console.log(res);
         window.alert('data berhasil di tambah');
-    }).catch( err => {
+    }).catch(err => {
         console.log(err);
     });
 });
 
-const hapus = id=>{
+const hapus = id => {
     axios.delete(`http://localhost:3000/contacts/${id}`)
 }
-const ganti = id =>{
-    const contacts = data.find(item=>{
+const ganti = id => {
+    const contacts = data.find(item => {
         return item.id === id
     })
-    if (contacts){
-        const name = window.prompt('Name',contacts.name);
-        const address = window.prompt('Address',contacts.address);
-        const email = window.prompt('Email',contacts.email);
-        const phone = window.prompt('Phone',contacts.phone);
-        const company = window.prompt('Company',contacts.Company);
-        axios.put(`http://localhost:3000/contacts/${id}`,{
+    if (contacts) {
+        const name = window.prompt('Name', contacts.name);
+        const address = window.prompt('Address', contacts.address);
+        const email = window.prompt('Email', contacts.email);
+        const phone = window.prompt('Phone', contacts.phone);
+        const company = window.prompt('Company', contacts.Company);
+        axios.put(`http://localhost:3000/contacts/${id}`, {
             name,
             address,
             email,
@@ -77,5 +103,5 @@ const ganti = id =>{
             company
         });
     }
-    
+
 }
